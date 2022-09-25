49ab665fd0a3458735762e83944a68a230a44162
Wilhelm Ågren <wilhelmagren98@gmail.com>
2022-09-25 14:42:20 +0200

    Copy over all testing experiments to new dir,  is reserved by Cargo.

--- src/lib.rs
+++ src/lib.rs
@@ -6,14 +6,36 @@ pub trait TensorType: Default + Debug {
 }
 
 #[allow(dead_code)]
-pub struct Tensor<T> {
+pub struct Tensor {
     dims: Vec<usize>,
-    data: Vec<T>,
+    data: Vec<f32>
 }
 
 #[allow(dead_code)]
-impl<T> Tensor<T> {
-    fn new(dims: Vec<usize>, data: Vec<T>) -> Self  {
+impl Tensor {
+    fn new(dims: Vec<usize>, data: Vec<f32>) -> Tensor {
         Tensor { dims, data }
+    }
+
+    fn zeros(dims: Vec<usize>) -> Tensor {
+        let size: usize = dims.iter().product();
+        Tensor { dims, data: vec![0.0; size] }
+    }
+
+    fn ones(dims: Vec<usize>) -> Tensor {
+        let size: usize = dims.iter().product();
+        Tensor { dims, data: vec![1.0; size] }
+    }
+}
+
+#[cfg(tests)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let t: Tensor = Tensor::new(vec![2, 3], vec![2.0; 6]);
+        assert_eq!(t.dims, vec![2, 3]);
+        assert_eq!(t.data, vec![2.0; 6]);
     }
 }
